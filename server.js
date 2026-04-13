26db38de2ed719d0e2ddcb790dcceff6be1be529
Rafaelprozx <42326761+Rafaelprozx@users.noreply.github.com>
2026-04-12 21:06:38 -0300

    minimun player lenght added

--- server.js
+++ server.js
@@ -446,8 +446,16 @@ socket.on("request_game_state", () => {
   socket.on('start_game', () => {
     const roomData = getRoomBySocket(socket.id);
     if (!roomData) return;
-
+	if(room.players.length < 2){
+		socket.emit('server_error', {
+        message: 'No se puede iniciar con menos de dos jugadores'
+      });
+	}
+	
     const { roomCode, room } = roomData;
+	
+	
+	
     if (room.hostId !== socket.id) {
       socket.emit('server_error', {
         message: 'Solo el host puede iniciar la partida.'
